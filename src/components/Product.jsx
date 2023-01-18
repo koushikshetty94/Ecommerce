fbb65bc410cab65e6e8f77898133b389ad488428
Koushik Shetty <koushikshetty@Koushiks-MacBook-Air.local>
2023-01-18 12:27:09 +0530

    Add individaul product list

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,77 @@
+import { FavoriteOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
+import styled from 'styled-components'
 
-const Product = () => {
+const Info = styled.p`
+    opacity: 0;
+    width:100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.2);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5s ease;
+    cursor:pointer; 
+`;
+
+const Container = styled.div`
+    flex:1;
+    margin: 5px;
+    height: 350px;
+    min-width : 280px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
+
+    &:hover ${Info}{
+        opacity: 0.5;
+    }
+`;
+
+const Image = styled.img`
+    height: 75%;
+    z-index: 2;
+`;
+
+const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+
+    &:hover{
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+    }
+        
+    `;
+
+const Product = ({item}) => {
   return (
-    <div>Product</div>
+    <Container>
+        <Image src={item.img}/>
+        <Info>
+            <Icon>
+                <ShoppingCartOutlined/>
+            </Icon>
+            <Icon>
+                <SearchOutlined/>
+            </Icon>
+            <Icon>
+                <FavoriteOutlined/>
+            </Icon>
+        </Info>
+    </Container>
   )
 }
-
 export default Product
